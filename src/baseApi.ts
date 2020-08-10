74d982f8d9bc413987225de18f4820dfb22e5cde
Federico Miras <miras.federico@gmail.com>
2020-08-10 03:50:30 -0300

    feat: automatically authenticate API with credentials

--- src/baseApi.ts
+++ src/baseApi.ts
@@ -3,13 +3,42 @@ import fetch from 'node-fetch'
 type QueryParameters = { [key: string]: number | number[] | string | string[] }
 
 export class BaseApi {
-  constructor(private apiKey: string, private baseUrl: string = 'https://api.pluggy.ai/v1') {}
+  private apiKey: string
+
+  constructor(
+    private clientId: string,
+    private clientSecret: string,
+    private baseUrl: string = 'https://api.pluggy.ai'
+  ) {}
+
+  private async getApiKey(): Promise<string> {
+    if (this.apiKey) {
+      return this.apiKey
+    }
+
+    const response = await fetch(`${this.baseUrl}/auth`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+        nonExpiring: false,
+      }),
+    })
+
+    const { apiKey } = await response.json()
+    this.apiKey = apiKey
+    return apiKey
+  }
 
   protected async createGetRequest<T>(endpoint: string, params?: QueryParameters): Promise<T> {
+    const apiKey = await this.getApiKey()
     return fetch(`${this.baseUrl}/${endpoint}${this.mapToQueryString(params)}`, {
       method: 'get',
       headers: {
-        'X-API-KEY': this.apiKey,
+        'X-API-KEY': apiKey,
       },
     })
       .then(async response => {
@@ -55,16 +84,18 @@ export class BaseApi {
     return this.createMutationRequest('patch', endpoint, params, body)
   }
 
-  protected createMutationRequest<T>(
+  protected async createMutationRequest<T>(
     method: string,
     endpoint: string,
     params?: QueryParameters,
     body?: any
   ): Promise<T> {
+    const apiKey = await this.getApiKey()
     return fetch(`${this.baseUrl}/${endpoint}${this.mapToQueryString(params)}`, {
       method,
       headers: {
-        'X-API-KEY': this.apiKey,
+        'X-API-KEY': apiKey,
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
     }).then(async response => {
