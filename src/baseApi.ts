267fb41e3cb21484455772d3185662f4237c46d0
Federico Miras <miras.federico@gmail.com>
2020-08-10 03:38:22 -0300

    chore: fix some linter warnings

--- src/baseApi.ts
+++ src/baseApi.ts
@@ -1,55 +1,73 @@
 import fetch from 'node-fetch'
 
+type QueryParameters = { [key: string]: number | number[] | string | string[] }
+
 export class BaseApi {
   constructor(private apiKey: string, private baseUrl: string = 'https://api.pluggy.ai/v1') {}
 
-  protected async createGetRequest(endpoint: string, params?: any) {
+  protected async createGetRequest<T>(endpoint: string, params?: QueryParameters): Promise<T> {
     return fetch(`${this.baseUrl}/${endpoint}${this.mapToQueryString(params)}`, {
       method: 'get',
       headers: {
-        'X-API-KEY': this.apiKey
-      }
+        'X-API-KEY': this.apiKey,
+      },
     })
-    .then(async (response) => {
-      try {
-        const json = await response.json()
-        if (response.status !== 200) {
-          return Promise.reject(json)
-        } else {
-          return Promise.resolve(json)
+      .then(async response => {
+        try {
+          const json = await response.json()
+          if (response.status !== 200) {
+            return Promise.reject(json)
+          } else {
+            return Promise.resolve(json)
+          }
+        } catch {
+          const message = await response.text()
+          return Promise.reject({ message })
         }
-      } catch {
-        const message = await response.text()
-        return Promise.reject({ message })
-      }
-    })
-    .catch((error) => {
+      })
+      .catch(error => {
         console.warn(`[API] HTTP request failed: ${error.message || ''}`, error)
         return Promise.reject(error)
-    })
+      })
   }
 
-  protected createPostRequest(endpoint: string, params?: any, body?: any) {
-    return this.createMutationRequest('post', endpoint, params, body);
+  protected createPostRequest<T>(
+    endpoint: string,
+    params?: QueryParameters,
+    body?: any
+  ): Promise<T> {
+    return this.createMutationRequest('post', endpoint, params, body)
   }
 
-  protected createPutRequest(endpoint: string, params?: any, body?: any) {
-    return this.createMutationRequest('put', endpoint, params, body);
+  protected createPutRequest<T>(
+    endpoint: string,
+    params?: QueryParameters,
+    body?: any
+  ): Promise<T> {
+    return this.createMutationRequest('put', endpoint, params, body)
   }
 
-  protected createPatchRequest(endpoint: string, params?: any, body?: any) {
-    return this.createMutationRequest('patch', endpoint, params, body);
+  protected createPatchRequest<T>(
+    endpoint: string,
+    params?: QueryParameters,
+    body?: any
+  ): Promise<T> {
+    return this.createMutationRequest('patch', endpoint, params, body)
   }
 
-  protected createMutationRequest(method: string, endpoint: string, params?: any, body?: any) {
+  protected createMutationRequest<T>(
+    method: string,
+    endpoint: string,
+    params?: QueryParameters,
+    body?: any
+  ): Promise<T> {
     return fetch(`${this.baseUrl}/${endpoint}${this.mapToQueryString(params)}`, {
       method,
       headers: {
-        'X-API-KEY': this.apiKey
+        'X-API-KEY': this.apiKey,
       },
-      body: JSON.stringify(body)
-    })
-    .then(async (response) => {
+      body: JSON.stringify(body),
+    }).then(async response => {
       try {
         const json = await response.json()
         if (response.status !== 200) {
@@ -64,7 +82,7 @@ export class BaseApi {
     })
   }
 
-  protected mapToQueryString(params: any): string {
+  protected mapToQueryString(params: QueryParameters): string {
     if (!params) {
       return ''
     }
@@ -73,5 +91,5 @@ export class BaseApi {
       .map(key => key + '=' + params[key])
       .join('&')
     return `?${query}`
-  }    
+  }
 }
