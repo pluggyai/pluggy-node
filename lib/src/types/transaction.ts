d2f44262d0f3db47003e5961169ca0818e948451
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2021-04-22 09:48:11 -0300

    feat: add paymentData to transactions

--- lib/src/types/transaction.ts
+++ lib/src/types/transaction.ts
@@ -1,6 +1,39 @@
 import { CurrencyCode } from './common'
 
 
+export type TransactionPaymentParticipantDocument = {
+  /** document number */
+  value?: string
+  /** Type of document provided, ie. CPF / CNPJ */
+  type?: 'CPF' | 'CNPJ'
+}
+
+export type TransactionPaymentParticipant = {
+  /** Document number object */
+  documentNumber?: TransactionPaymentParticipantDocument
+  /** Name of the participant */
+  name?: string
+  /** Number of the account */
+  accountNumber?: string
+  /** Number of the agency / branch */
+  branchNumber?: string
+  /** Number of the bank */
+  routingNumber?: string
+}
+
+export type TransactionPaymentData = {
+  /** The identity of the sender of the transfer */
+  payer?: TransactionPaymentParticipant
+  /** The identity of the receiver of the transfer */
+  receiver?: TransactionPaymentParticipant
+  /** Identifier for the transaction provided by the institution */
+  paymentMethod?: string
+  /** The type of transfer used "PIX", "TED", "DOC". */
+  referenceNumber?: string
+  /** The payer description / motive of the transfer */
+  reason?: string
+}
+
 export type TransactionFilters = {
   /** Filter greater than date. Format (ISO Date | yyyy-mm-dd) */
   to?: string
@@ -31,4 +64,6 @@ export type Transaction = {
   category?: string
   /** Code provided by the financial institution for the transaction type, not unique. */
   providerCode?: string
+  /** Additional data related to payment or transfers */
+  paymentData?: TransactionPaymentData
 }
