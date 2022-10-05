14326bf4ddb6ba088bb24a4ec7bdf454db487075
NicolasMontone <montonenicolas01@gmail.com>
2022-10-05 09:38:33 -0300

    chore(webhookk): fix comments.

--- src/types/webhook.ts
+++ src/types/webhook.ts
@@ -15,27 +15,27 @@ export const WEBHOOK_EVENTS = [
 export type WebhookEvent = typeof WEBHOOK_EVENTS[number]
 
 export type Webhook = {
-  /*! Primary identifier of the entity */
+  /** Primary identifier of the entity */
   id: string
-  /*! Type of event subscribed */
+  /** Type of event subscribed */
   event: WebhookEvent
-  /*! Url where notifications of events will be sent */
+  /** Url where notifications of events will be sent */
   url: string
-  /*! Object to specify headers in your webhook notifications */
+  /** Object to specify headers in your webhook notifications */
   headers: Record<string, string>
 }
 
 export type WebhookEventPayload = {
-  /*! Primary identifier of the resource that was notified */
+  /** Primary identifier of the resource that was notified */
   id: string
-  /*! Type of event subscribed */
+  /** Type of event subscribed */
   event: WebhookEvent
-  /*! Url where notifications of events will be sent */
+  /** Url where notifications of events will be sent */
   url?: string
-  /*! Primary identifier of the item related to the event */
+  /** Primary identifier of the item related to the event */
   itemId?: string
-  /*! Object related to item/error event */
+  /** Object related to item/error event */
   error?: Record<string, unknown>
-  /*! Object to specify headers in webhook notifications */
+  /** Object to specify headers in webhook notifications */
   headers?: Record<string, string>
 }
