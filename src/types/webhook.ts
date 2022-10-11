4dc12b364c956a6762cf0f458304955fc4083da2
NicolasMontone <montonenicolas01@gmail.com>
2022-10-11 14:47:23 -0300

    fix: webhook types and fix methods definitions.

--- src/types/webhook.ts
+++ src/types/webhook.ts
@@ -17,6 +17,20 @@ export type WebhookEvent = typeof WEBHOOK_EVENTS[number]
 export type Webhook = {
   /** Primary identifier of the entity */
   id: string
+  /** Url where notifications of events will be sent */
+  url: string
+  /** Type of event subscribed */
+  event: WebhookEvent
+  /** Time of the creation of the webhook */
+  createdAt: Date
+  /** Time of last time the webhook was updated
+   * (note: if it was never updated it will be equal to createdAt) */
+  updatedAt: Date
+  /** Time of when the webhook was disabled */
+  disabledAt: Date | null
+}
+
+export type CreateWebhook = {
   /** Type of event subscribed */
   event: WebhookEvent
   /** Url where notifications of events will be sent */
@@ -28,14 +42,50 @@ export type Webhook = {
 export type WebhookEventPayload = {
   /** Primary identifier of the resource that was notified */
   id: string
-  /** Type of event subscribed */
-  event: WebhookEvent
-  /** Url where notifications of events will be sent */
-  url?: string
-  /** Primary identifier of the item related to the event */
-  itemId?: string
-  /** Object related to item/error event */
-  error?: Record<string, unknown>
-  /** Object with extra information such as the new Connector Status */
-  data?: Record<string, unknown>
-}
+} & (
+  | {
+      /** Type of event subscribed */
+      event:
+        | 'item/created'
+        | 'item/updated'
+        | 'item/waiting_user_input'
+        | 'item/login_succeeded'
+        | 'item/deleted'
+      /** Primary identifier of the item related to the event */
+      itemId: string
+    }
+  | {
+      /** Type of event subscribed */
+      event: 'item/error'
+      /** Primary identifier of the item related to the event */
+      itemId: string
+      /** Object related to item/error event */
+      error: {
+        code: string
+        message: string
+      }
+    }
+  | {
+      /** Type of event subscribed */
+      event: 'connector/status_updated'
+      /** Object with extra information of the connector updated */
+      data: {
+        status: string
+      }
+    }
+  | {
+      /** Type of event subscribed */
+      event: 'all'
+      /** Primary identifier of the item related to the event */
+      itemId?: string
+      /** Object with extra information of the connector updated */
+      data?: {
+        status: string
+      }
+      /** Object related to item/error event */
+      error?: {
+        code: string
+        message: string
+      }
+    }
+)
