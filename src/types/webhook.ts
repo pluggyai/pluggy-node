64399ec56401179701ba2eacf73738ef6b4287d9
NicolasMontone <montonenicolas01@gmail.com>
2022-10-14 12:46:28 -0300

    fix(webhook): set triggerBy as a common property in the type since it could be optional in all events.

--- src/types/webhook.ts
+++ src/types/webhook.ts
@@ -44,6 +44,8 @@ export type CreateWebhook = {
 export type WebhookEventPayload = {
   /** Primary identifier of the resource that was notified */
   id: string
+  /**  */
+  triggerBy?: TriggeredBy | null
 } & (
   | {
       /** Type of event subscribed */
@@ -55,7 +57,6 @@ export type WebhookEventPayload = {
         | 'item/deleted'
       /** Primary identifier of the item related to the event */
       itemId: string
-      triggerBy?: TriggeredBy | null
     }
   | {
       /** Type of event subscribed */
@@ -67,7 +68,6 @@ export type WebhookEventPayload = {
         code: string
         message: string
       }
-      triggerBy?: TriggeredBy | null
     }
   | {
       /** Type of event subscribed */
