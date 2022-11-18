9446476cf254fe01e31c58281295eb95b1680f90
Tomás Milar <tomasmilar4@gmail.com>
2022-11-18 15:01:07 -0300

    fix(example): fix MFA example with latest API & Item type definitions.

--- example/src/mfa.ts
+++ example/src/mfa.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv'
 import { PluggyClient } from 'pluggy-sdk'
-import { ItemStatus } from 'pluggy-sdk/dist/types'
-import { sleep, PLUGGY_BANK_CREDENTIALS, PLUGGY_BANK_CONNECTOR } from './utils'
+import {
+  PLUGGY_BANK_CONNECTOR,
+  PLUGGY_BANK_CREDENTIALS,
+  PLUGGY_BANK_MFA_2STEP_CONNECTOR,
+  sleep,
+} from './utils'
+import { Item, ItemStatus } from '../../src'
 
 const DEFAULT_MFA_VALUE = '123456'
 
@@ -16,31 +21,53 @@ void (async function(): Promise<void> {
   })
 
   // Create a connection
-  let item = await client.createItem(PLUGGY_BANK_CONNECTOR, {
-    ...PLUGGY_BANK_CREDENTIALS,
-    user: 'user-mfa',
-  })
+  let item: Item = await client.createItem(PLUGGY_BANK_MFA_2STEP_CONNECTOR, PLUGGY_BANK_CREDENTIALS)
 
-  while (![ItemStatus.LOGIN_ERROR, ItemStatus.OUTDATED, ItemStatus.UPDATED].includes(item.status)) {
-    console.log(`Item ${item.id} its syncing with the institution, current status ${item.status}`)
-    await sleep(3000)
-    item = await client.fetchItem(item.id)
-    if (item.status === ItemStatus.WAITING_USER_INPUT) {
-      const { parameter } = item
-      console.log(`There is a MFA requested, ${parameter.name}, providing value.`)
-      try {
-        item = await client.updateItemMFA(item.id, {
-          [parameter.name]: DEFAULT_MFA_VALUE,
-        })
-      } catch (err) {
-        console.error(err)
+  while (item.status === 'UPDATING' || item.status === 'WAITING_USER_INPUT') {
+    const { status, parameter } = item
+
+    console.log(
+      new Date().toISOString(),
+      `Item ${item.id} is syncing with the institution, current status ${status}`
+    )
+
+    if (status === 'UPDATING') {
+      // wait & fetch Item again, to re-check status
+      await sleep(3000)
+      item = await client.fetchItem(item.id)
+      continue
+    }
+
+    // status === 'WAITING_USER_INPUT'
+
+    if (!parameter) {
+      console.log(
+        new Date().toISOString(),
+        'Item is in status WAITING_USER_INPUT but parameter is missing, fetching data again...'
+      )
+      continue
+    }
+
+    console.log(
+      new Date().toISOString(),
+      `There is a MFA requested, ${parameter.name}, providing value.`
+    )
+
+    try {
+      item = await client.updateItemMFA(item.id, {
+        [parameter.name]: DEFAULT_MFA_VALUE,
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        error.message = `Could not update Item MFA: ${error.message}`
+        console.error(new Date().toISOString(), error)
       }
     }
   }
 
-  console.log(`Item completed execution with status ${item.status}`)
+  console.log(new Date().toISOString(), `Item completed execution with status ${item.status}`)
 
-  console.log(`Deleting retrieved data for item #${item.id}`)
+  console.log(new Date().toISOString(), `Deleting retrieved data for item #${item.id}`)
   await client.deleteItem(item.id)
-  console.log(`Item deleted succesfully`)
+  console.log(new Date().toISOString(), `Item deleted succesfully`)
 })()
