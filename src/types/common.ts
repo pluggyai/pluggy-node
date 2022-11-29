1137a5e7cb02fdf6375de8add8b7543465312e32
NicolasMontone <montonenicolas01@gmail.com>
2022-11-29 15:18:53 -0300

    fix(common): add CountryCode type.

--- src/types/common.ts
+++ src/types/common.ts
@@ -16,6 +16,14 @@ export const CURRENCY_CODES = [
  */
 export type CurrencyCode = typeof CURRENCY_CODES[number]
 
+export const COUNTRY_CODES = ['AR', 'BR', 'CO', 'MX'] as const
+/**
+ * @typedef CountryCode
+ *  Supported and available countries, in ISO-3166-1 alpha 2 format.
+ *  Useful to filter connectors list.
+ */
+export type CountryCode = typeof COUNTRY_CODES[number]
+
 export type PageResponse<T> = {
   results: T[]
   page: number
