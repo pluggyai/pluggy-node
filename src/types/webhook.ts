765bc714c4317b42228145bbb2908e265d3c5732
sebipapsu <sebipaps@gmail.com>
2023-03-31 11:41:22 -0300

    fix(webhook): improve comment.

--- src/types/webhook.ts
+++ src/types/webhook.ts
@@ -50,7 +50,7 @@ export type UpdateWebhook = Partial<CreateWebhook> & {
 export type WebhookEventPayload = {
   /** @deprecated id of the entity related to the event (use eventId to uniquely identify a webhook event)*/
   id: string
-  /** Primary identifier of the resource that was notified */
+  /** Primary identifier of the event that was notified */
   eventId: string
 } & (
   | {
