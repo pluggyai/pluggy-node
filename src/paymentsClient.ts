d49c99e890e34b63491b60bc82f637d8fac75ac3
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2023-12-12 11:13:34 +0100

    feat: add new endpoints for customers, institutions and recipients

--- src/paymentsClient.ts
+++ src/paymentsClient.ts
@@ -7,6 +7,15 @@ import {
   PaymentIntentsFilters,
   PaymentRequest,
   PaymentRequestsFilters,
+  PaymentCustomer,
+  CreatePaymentCustomer,
+  PaymentCustomersFilters,
+  CreatePaymentRecipient,
+  PaymentRecipient,
+  PaymentRecipientsFilters,
+  UpdatePaymentRecipient,
+  PaymentInstitution,
+  PaymentInstitutionsFilters,
 } from './types'
 /**
  * Creates a new client instance for interacting with Pluggy API for the Payments API
@@ -72,5 +81,109 @@ export class PluggyPaymentsClient extends BaseApi {
     options: PaymentIntentsFilters = {}
   ): Promise<PageResponse<PaymentIntent>> {
     return this.createGetRequest('payments/intents', { ...options })
+  }
+
+  /**
+   * Creates a payment customer
+   * @returns {PaymentCustomer} PaymentCustomer object
+   */
+  async createPaymentCustomer(payload: CreatePaymentCustomer): Promise<PaymentCustomer> {
+    return this.createPostRequest(`payments/customers`, null, payload)
+  }
+
+  /**
+   * Fetch a single payment customer
+   * @returns {PaymentCustomer} PaymentCustomer object
+   */
+  async fetchPaymentCustomer(id: string): Promise<PaymentCustomer> {
+    return this.createGetRequest(`payments/customers/${id}`)
+  }
+
+  /**
+   * Fetch all payment customers
+   * @returns {PageResponse<PaymentCustomer>} paged response of payment customers
+   */
+  async fetchPaymentCustomers(
+    options: PaymentCustomersFilters = {}
+  ): Promise<PageResponse<PaymentCustomer>> {
+    return this.createGetRequest('payments/customers', { ...options })
+  }
+
+  /**
+   * Delete a payment customer
+   */
+  async deletePaymentCustomer(id: string): Promise<void> {
+    await this.createDeleteRequest(`payments/customers/${id}`)
+  }
+
+  async updatePaymentCustomer(
+    id: string,
+    payload: Partial<CreatePaymentCustomer>
+  ): Promise<PaymentCustomer> {
+    return this.createPatchRequest(`payments/customers/${id}`, null, payload)
+  }
+
+  /**
+   * Creates a payment recipient (bank account)
+   * @returns {PaymentRecipient} PaymentRecipient object
+   */
+  async createPaymentRecipient(payload: CreatePaymentRecipient): Promise<PaymentRecipient> {
+    return this.createPostRequest(`payments/recipients`, null, payload)
+  }
+
+  /**
+   * Fetch a single payment recipient
+   * @returns {PaymentRecipient} PaymentRecipient object
+   */
+  async fetchPaymentRecipient(id: string): Promise<PaymentRecipient> {
+    return this.createGetRequest(`payments/recipients/${id}`)
+  }
+
+  /**
+   * Fetch all payment recipients
+   * @returns {PageResponse<PaymentRecipient>} paged response of payment recipients
+   */
+  async fetchPaymentRecipients(
+    options: PaymentRecipientsFilters = {}
+  ): Promise<PageResponse<PaymentRecipient>> {
+    return this.createGetRequest('payments/recipients', { ...options })
+  }
+
+  /**
+   * Delete a payment recipient
+   */
+  async deletePaymentRecipient(id: string): Promise<void> {
+    await this.createDeleteRequest(`payments/recipients/${id}`)
+  }
+
+  /**
+   * Update a payment recipient
+   * @param id ID of the payment recipient
+   * @param payload Fields to update
+   * @returns {PaymentRecipient} PaymentRecipient object
+   */
+  async updatePaymentRecipient(
+    id: string,
+    payload: UpdatePaymentRecipient
+  ): Promise<PaymentRecipient> {
+    return this.createPatchRequest(`payments/recipients/${id}`, null, payload)
+  }
+
+  /**
+   * Fetch a single payment recipient institution
+   * @returns {PaymentInstitution} PaymentInstitution object
+   */
+  async fetchPaymentInstitution(id: string): Promise<PaymentInstitution> {
+    return this.createGetRequest(`payments/recipients/institutions/${id}`)
+  }
+
+  /**
+   * Fetch all payment recipient institutions
+   * @returns {PageResponse<PaymentInstitution>} paged response of payment recipient institutions
+   */
+  async fetchPaymentInstitutions(
+    options: PaymentInstitutionsFilters
+  ): Promise<PageResponse<PaymentInstitution>> {
+    return this.createGetRequest('payments/recipients/institutions', { ...options })
   }
 }
--- src/types/payments.ts
+++ src/types/payments.ts
@@ -150,3 +150,5 @@ export type PaymentIntentsFilters = PageFilters & DateFilters
 export type PaymentRecipientsFilters = PageFilters
 
 export type PaymentCustomersFilters = PageFilters
+
+export type PaymentInstitutionsFilters = PageFilters & { name?: string }
