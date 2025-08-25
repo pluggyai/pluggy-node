00a7553ac921888821ee84ff71af3dc7160e2948
Francisco Disalvo <francisco.disalvo90@gmail.com>
2025-08-25 16:41:09 -0300

    feat: delete unnecesary type

--- src/types/accountStatement.ts
+++ src/types/accountStatement.ts
@@ -1,17 +1,3 @@
-import { CurrencyCode } from './common'
-
-export type AccountStatement = {
-  id: string
-  dueDate: Date
-  totalAmount: number
-  totalAmountCurrencyCode: CurrencyCode
-  minimumPaymentAmount: number | null
-  allowsInstallments: boolean | null
-  financeCharges: CreditCardBillFinanceChargeResponseItem[]
-  createdAt: Date
-  updatedAt: Date
-}
-
 export type CreditCardBillFinanceChargeResponseItem = {
   /*! Primary identifier of the entity */
   id: string
